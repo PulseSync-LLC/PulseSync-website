8196aefedcef27ea65a8df1c66b928744a9063bb
Svyatoslav <zerotwo1337@inbox.ru>
2024-05-28 00:13:42 +0300

    declaration for sass
    Заняло 4 minutes

---
+++ declarations.d.ts
@@ -0,0 +1,9 @@
+declare module '*.module.scss' {
+    const content: { [key: string]: string };
+    export default content;
+}
+
+declare module '*.module.css' {
+    const content: { [key: string]: string };
+    export default content;
+}
