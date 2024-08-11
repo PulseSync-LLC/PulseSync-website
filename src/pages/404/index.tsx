8baeca2c63290e59b78ad0fcf72e7527f698cebf
EvT <one.evt@gmail.com>
2024-08-12 01:20:18 +0300

    added grid on 404

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -3,12 +3,12 @@ import styles from "@/styles/404.module.scss";
 import { useEffect, useState } from "react";
 
 export default function NotFound() {
-    const [backgroundColor, setBackgroundColor] = useState('');
+    const [background, setBackground] = useState('');
     const [imageSrc, setImageSrc] = useState('/assets/img/404.png');
 
     useEffect(() => {
         const randomHue = Math.floor(Math.random() * 360);
-        setBackgroundColor(`hsl(${randomHue}, 29%, 27%)`);
+        setBackground(`url(/assets/img/grid.png), hsl(${randomHue}, 29%, 27%)`);
 
         const randomChance = Math.random();
         if (randomChance <= 0.05) {
@@ -17,7 +17,7 @@ export default function NotFound() {
     }, []);
 
     return (
-        <div className={styles.background} style={{ backgroundColor }}>
+        <div className={styles.background} style={{ background }}>
             <Image
                 src={imageSrc}
                 width={720.21}
